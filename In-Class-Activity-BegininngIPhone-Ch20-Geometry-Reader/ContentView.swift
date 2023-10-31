289dc00c73af7f412e0f940766956ec6fb20bfaa
Tori Lew <55900873+Tori-Lew@users.noreply.github.com>
2023-10-30 20:39:15 -0700

    Initial commit

---
+++ In-Class-Activity-BegininngIPhone-Ch20-Geometry-Reader/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  In-Class-Activity-BegininngIPhone-Ch20-Geometry-Reader
+//
+//  Created by Student Account on 10/30/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ In-Class-Activity-BegininngIPhone-Ch20-Geometry-Reader/In_Class_Activity_BegininngIPhone_Ch20_Geometry_ReaderApp.swift
@@ -0,0 +1,17 @@
+//
+//  In_Class_Activity_BegininngIPhone_Ch20_Geometry_ReaderApp.swift
+//  In-Class-Activity-BegininngIPhone-Ch20-Geometry-Reader
+//
+//  Created by Student Account on 10/30/23.
+//
+
+import SwiftUI
+
+@main
+struct In_Class_Activity_BegininngIPhone_Ch20_Geometry_ReaderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
